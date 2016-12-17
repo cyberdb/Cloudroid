6b232bcf3e374a97842e1e6dfb016240fae296b7
xiteen <siteen@outlook.com>
2016-12-17 11:34:44 +0800

    test case

--- app/templates/ros_entry.sh
+++ app/templates/ros_entry.sh
@@ -3,6 +3,8 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/catkin_install/devel/setup.bash"
+export ROS_HOSTNAME=localhost
+export ROS_MASTER_URL=http://localhost:11311
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/catkin_install/devel/src
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/catkin_install/src
 
