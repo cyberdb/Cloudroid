cfba3716c19b6fea97f64a96f8a59b4e86ba5c3e
Pengfei Zhang <zpfalpc23@gmail.com>
2017-05-30 19:52:04 +0800

    Enable the ros catkin build with isolated env

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ COPY ./buildimages.sh /
 
 RUN chmod +x /ros_entry.sh
 RUN chmod +x /rosdep.py
-RUN chmod +x /catkin_install/devel/_setup_util.py
+#RUN /bin/bash -c 'chmod +x /catkin_install/devel/_setup_util.py; exit 0'
+#RUN /bin/bash -c 'chmod +x /catkin_install/install_isolated/_setup_util.py; exit 0'
 RUN chmod +x /buildimages.sh
 
 RUN ./rosdep.py
--- buildimages.sh
+++ buildimages.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
+
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/catkin_install/devel/setup.bash"
+source "/catkin_install/devel/setup.bash" || true
+source "/catkin_install/install_isolated/setup.bash" || true
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/catkin_install/devel/src
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/catkin_install/install_isolated/src
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/catkin_install/src
+
 
 apt-get update
 
-rosdep install $1 -y
+rosdep install --from-paths /catkin_install/src --ignore-src --rosdistro=$ROS_DISTRO -y
